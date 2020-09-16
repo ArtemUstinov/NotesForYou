0d7c47d58aac86f4e9cffddab5bfdc1fad9cfaed
ArtemUstinov <arti-0@mail.ru>
2020-09-16 18:16:30 +0300

    Initial Commit

---
+++ NotesForYou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesForYou
+//
+//  Created by Артём Устинов on 16.09.2020.
+//  Copyright © 2020 Artem Ustinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
