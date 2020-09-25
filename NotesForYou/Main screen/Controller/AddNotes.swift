5aee8537cb4377f8cc85fa460dc3655f1c918950
ArtemUstinov <arti-0@mail.ru>
2020-09-25 20:31:29 +0300

    For example

--- NotesForYou/Main screen/Controller/AddNotes.swift
+++ NotesForYou/Main screen/Controller/AddNotes.swift
@@ -6,4 +6,124 @@
 //  Copyright © 2020 Artem Ustinov. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class AddNotes {
+    
+    let mainVC = UIApplication.shared.delegate as? MainTableViewController
+
+    
+     func setupAlert() {
+            
+            let alertController = UIAlertController(title: "New note", message: "Please add new note", preferredStyle: .alert)
+            
+            let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+                let textField = alertController.textFields?.first
+                if let newNote = textField?.text {
+
+                    switch self.mainVC?.segmentedControl.selectedSegmentIndex {
+                    case 0:
+    //                    self.saveBook(withTitle: newNote)
+                        self.saveBook(withTitle: newNote)
+                        break
+                    case 1:
+    //                    self.saveFilm(withTitle: newNote)
+                        self.saveFilm(withTitle: newNote)
+                        break
+                    case 2:
+    //                    self.saveMusic(withTitle: newNote)
+                        self.saveMusic(withTitle: newNote)
+                        break
+                    default:
+                        break
+                    }
+                    self.mainVC?.tableView.reloadData()
+                }
+            }
+            
+            alertController.addTextField { (textField) in
+                textField.autocapitalizationType = .sentences
+                textField.returnKeyType = .done
+                
+                switch self.mainVC?.segmentedControl.selectedSegmentIndex {
+                case 0:
+                    textField.placeholder = "Book"
+                    break
+                case 1:
+                    textField.placeholder = "Film"
+                    break
+                case 2:
+                    textField.placeholder = "Music"
+                    break
+                default:
+                    break
+                }
+                self.mainVC?.tableView.reloadData()
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
+            
+            alertController.addAction(saveAction)
+            alertController.addAction(cancelAction)
+            
+        mainVC?.present(alertController, animated: true)
+        }
+    
+    
+     func saveBook(withTitle title: String) {
+        let context = getContext()
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Book", in: context) else { return }
+        
+        let noteObject = Book(entity: entity, insertInto: context)
+        noteObject.title = title
+        
+        do {
+            try context.save()
+//            books.append(noteObject)
+            mainVC?.books.append(noteObject)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+     func saveFilm(withTitle title: String) {
+        let context = getContext()
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Film", in: context) else { return }
+        
+        let noteObject = Film(entity: entity, insertInto: context)
+        noteObject.title = title
+        
+        do {
+            try context.save()
+//            films.append(noteObject)
+            mainVC?.films.append(noteObject)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+     func saveMusic(withTitle title: String) {
+        let context = getContext()
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Music", in: context) else { return }
+        
+        let noteObject = Music(entity: entity, insertInto: context)
+        noteObject.title = title
+        
+        do {
+            try context.save()
+//            musics.append(noteObject)
+            mainVC?.musics.append(noteObject)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+     private func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+}
--- NotesForYou/Main screen/Controller/MainTableViewController.swift
+++ NotesForYou/Main screen/Controller/MainTableViewController.swift
@@ -7,27 +7,33 @@
 //
 
 import UIKit
+import CoreData
 
 class MainTableViewController: UITableViewController {
-        
-    var books = [String]()
-    var films = [String]()
-    var musics = [String]()
+    
+    var books = [Book]()
+    var films = [Film]()
+    var musics = [Music]()
+    
+    let addNotes = UIApplication.shared.delegate as? AddNotes
+    let addNote = AddNotes()
+    
     
     //MARK:- IBOutlet:
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
-    @IBOutlet weak var nameLabel: UILabel!
-    
-    @IBOutlet weak var descriptionLabel: UILabel!
-    
-    
+
     //MARK:- IBActions:
     
-    @IBAction func saveNote(_ sender: UIBarButtonItem) {
+    @IBAction func addNote(_ sender: UIBarButtonItem) {
         
-        setupAlert()
+//        if let setupAlert = addNotes?.setupAlert() {
+//            return setupAlert
+//        } else {
+//            return print("error")
+//        }
+        addNote.setupAlert()
+        
     }
     
     @IBAction func scPressed(_ sender: UISegmentedControl) {
@@ -35,84 +41,89 @@ class MainTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
-     
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let context = getContext()
+        let fetchRequestOfBook: NSFetchRequest<Book> = Book.fetchRequest()
+        let fetchRequestOfFilm: NSFetchRequest<Film> = Film.fetchRequest()
+        let fetchRequestOfMusic: NSFetchRequest<Music> = Music.fetchRequest()
+        
+        do {
+            books = try context.fetch(fetchRequestOfBook)
+            films = try context.fetch(fetchRequestOfFilm)
+            musics = try context.fetch(fetchRequestOfMusic)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.tableFooterView = UIView()
         setupSC()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-    
-    
-    private func settingSC() {
 
     }
     
     
-    private func setupAlert() {
-        
-        let alertController = UIAlertController(title: "New note", message: "Please add new note", preferredStyle: .alert)
-        
-        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
-            let textField = alertController.textFields?.first
-//            let textField2 = alertController.textFields![1]
-            if let newNote = textField?.text {
-              
-                switch self.segmentedControl.selectedSegmentIndex {
-                case 0:
-                    self.books.append(newNote)
-//                    self.books.append(newNote2)
-                    break
-                case 1:
-                    self.films.append(newNote)
-//                    self.films.append(newNote2)
-                    break
-                case 2:
-                    self.musics.append(newNote)
-//                    self.musics.append(newNote2)
-                    break
-                default:
-                    break
-                }
-                self.tableView.reloadData()
-            }
-        }
-        
-        alertController.addTextField { (textField) in
-            switch self.segmentedControl.selectedSegmentIndex {
-            case 0:
-                textField.placeholder = "Book"
-                break
-            case 1:
-                textField.placeholder = "Film"
-                break
-            case 2:
-                textField.placeholder = "Music"
-                break
-            default:
-                break
-            }
-            self.tableView.reloadData()
-        }
-//        textField.placeholder = "Book, movie, music"
-    
-    //        alertController.addTextField { (textField) in
-    //            textField.placeholder = "Description"
-    //        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
-        
-        alertController.addAction(saveAction)
-        alertController.addAction(cancelAction)
-        
-        present(alertController, animated: true)
-    }
-    
+//    private func setupAlert() {
+//        
+//        let alertController = UIAlertController(title: "New note", message: "Please add new note", preferredStyle: .alert)
+//        
+//        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+//            let textField = alertController.textFields?.first
+//            if let newNote = textField?.text {
+//                
+//                switch self.segmentedControl.selectedSegmentIndex {
+//                case 0:
+////                    self.saveBook(withTitle: newNote)
+//                    self.addNotes?.saveBook(withTitle: newNote)
+//                    break
+//                case 1:
+////                    self.saveFilm(withTitle: newNote)
+//                    self.addNotes?.saveFilm(withTitle: newNote)
+//                    break
+//                case 2:
+////                    self.saveMusic(withTitle: newNote)
+//                    self.addNotes?.saveMusic(withTitle: newNote)
+//                    break
+//                default:
+//                    break
+//                }
+//                self.tableView.reloadData()
+//            }
+//        }
+//        
+//        alertController.addTextField { (textField) in
+//            textField.autocapitalizationType = .sentences
+//            textField.returnKeyType = .done
+//            
+//            switch self.segmentedControl.selectedSegmentIndex {
+//            case 0:
+//                textField.placeholder = "Book"
+//                break
+//            case 1:
+//                textField.placeholder = "Film"
+//                break
+//            case 2:
+//                textField.placeholder = "Music"
+//                break
+//            default:
+//                break
+//            }
+//            self.tableView.reloadData()
+//        }
+//        
+//        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
+//        
+//        alertController.addAction(saveAction)
+//        alertController.addAction(cancelAction)
+//        
+//        present(alertController, animated: true)
+//    }
+//    
     private func setupSC() {
         segmentedControl.removeAllSegments()
         segmentedControl.insertSegment(withTitle: "Book", at: 0, animated: true)
@@ -120,18 +131,68 @@ class MainTableViewController: UITableViewController {
         segmentedControl.insertSegment(withTitle: "Film", at: 1, animated: true)
         segmentedControl.insertSegment(withTitle: "Music", at: 2, animated: true)
     }
-
+//    
+//    private func saveBook(withTitle title: String) {
+//        let context = getContext()
+//        
+//        guard let entity = NSEntityDescription.entity(forEntityName: "Book", in: context) else { return }
+//        
+//        let noteObject = Book(entity: entity, insertInto: context)
+//        noteObject.title = title
+//        
+//        do {
+//            try context.save()
+//            books.append(noteObject)
+//        } catch let error as NSError {
+//            print(error.localizedDescription)
+//        }
+//    }
+//    
+//    private func saveFilm(withTitle title: String) {
+//        let context = getContext()
+//        
+//        guard let entity = NSEntityDescription.entity(forEntityName: "Film", in: context) else { return }
+//        
+//        let noteObject = Film(entity: entity, insertInto: context)
+//        noteObject.title = title
+//        
+//        do {
+//            try context.save()
+//            films.append(noteObject)
+//        } catch let error as NSError {
+//            print(error.localizedDescription)
+//        }
+//    }
+//    
+//    private func saveMusic(withTitle title: String) {
+//        let context = getContext()
+//        
+//        guard let entity = NSEntityDescription.entity(forEntityName: "Music", in: context) else { return }
+//        
+//        let noteObject = Music(entity: entity, insertInto: context)
+//        noteObject.title = title
+//        
+//        do {
+//            try context.save()
+//            musics.append(noteObject)
+//        } catch let error as NSError {
+//            print(error.localizedDescription)
+//        }
+//    }
+//    
+    private func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-//        return generic.count
-
         switch segmentedControl.selectedSegmentIndex {
         case 0:
             return books.count
@@ -143,73 +204,106 @@ class MainTableViewController: UITableViewController {
             return 0
         }
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-//        cell.textLabel?.text = generic[indexPath.row]
         
-
+        //        let noteForBook = books[indexPath.row]
+        //        let noteForFilm = films[indexPath.row]
+        //        let noteForMusic = musics[indexPath.row]
+        
         switch segmentedControl.selectedSegmentIndex {
         case 0:
-             cell.textLabel?.text = books[indexPath.row]
+            cell.textLabel?.text = books[indexPath.row].title
         case 1:
-             cell.textLabel?.text = films[indexPath.row]
+            cell.textLabel?.text = films[indexPath.row].title
         case 2:
-             cell.textLabel?.text = musics[indexPath.row]
+            cell.textLabel?.text = musics[indexPath.row].title
         default:
             break
         }
-        
         return cell
     }
     
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let context = getContext()
+            switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                let book = books[indexPath.row]
+                books.remove(at: indexPath.row)
+                context.delete(book)
+                try! context.save()
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            case 1:
+                let book = films[indexPath.row]
+                films.remove(at: indexPath.row)
+                context.delete(book)
+                try! context.save()
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            case 2:
+                let book = musics[indexPath.row]
+                musics.remove(at: indexPath.row)
+                context.delete(book)
+                try! context.save()
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            default:
+                break
+            }
+        }
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
+
